be2b0462c56775730f53ba2889fa19266a1ac6ed
LSJ0621 <99dlatjdwo@gmail.com>
2025-09-05 00:10:55 +0900

    Update A504땅따먹기.java

--- src/A5DP/A504땅따먹기.java
+++ src/A5DP/A504땅따먹기.java
@@ -28,3 +28,49 @@ public class A504땅따먹기 {
         System.out.println(max);
     }
 }
+
+// 땅따먹기 dfs로 풀었을때 풀이, dfs방식을 잘 보자.
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.*;
+
+// public class Main {
+//     static int count;
+//     static int answer;
+//     public static void main(String[] args) throws IOException {
+// //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int[][] land = {
+//                 {1,2,3,5},
+//                 {5,6,7,8},
+//                 {4,3,2,1}
+//         };
+//         boolean[][] visited = new boolean[land.length][4];
+//         dfs(land,visited,0);
+//         System.out.println(answer);
+//     }
+
+//     static void dfs(int[][] land, boolean[][]visited,int n){
+//         if(n== land.length){
+//             if(count>answer){
+//                 answer = count;
+//             }
+//             return;
+//         }
+//         for(int i=0; i< 4;i++){
+//             if(!visited[n][i]){
+//                 count+=land[n][i];
+//                 if(n<land.length-1){
+//                     visited[n+1][i] = true;
+//                 }
+//                 dfs(land,visited,n+1);
+//                 if(n<land.length-1){
+//                     visited[n+1][i]=false;
+//                 }
+//                 count-=land[n][i];
+//             }
+//         }
+//     }
+
+
+// }

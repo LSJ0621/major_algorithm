919acf2ea13cda20deb9fde31a527afbf1aac9a9
LSJ0621 <99dlatjdwo@gmail.com>
2025-09-05 13:30:21 +0900

    Update A602연속된부분수열의합.java

--- src/A6투포인터/A602연속된부분수열의합.java
+++ src/A6투포인터/A602연속된부분수열의합.java
@@ -6,40 +6,26 @@ import java.util.List;
 
 public class A602연속된부분수열의합 {
     public static void main(String[] args) {
-        int[] sequence = {1, 2, 3, 4, 5};
-        int start = 0;
+        int[] sequence = {1, 1, 1, 2, 3, 4, 5};
+        int k = 	5;
+        int start =0;
         int end = 0;
-        int target = 7;
-        int total = sequence[0];
-        List<int[]> list = new ArrayList<>();
-        while (start <= end && end<sequence.length){
-            if(total == target){
-                list.add(new int[]{start, end});
-                total -= sequence[start];
-                start++;
-            }else if(total > target){
-                total -= sequence[start];
-                start++;
-            }else if(total < target){
-                if(end<sequence.length-1){
-                    end++;
-                    total += sequence[end];
-                }else {
-                    end++;
-                }
-            }
-        }
-        int min = Integer.MAX_VALUE;
-        for(int[] a : list){
-            if(a[1]-a[0] < min){
-                min = a[1]-a[0];
-            }
-        }
+        int total=0;
         int[] answer = new int[2];
-        for(int[] a : list){
-            if(a[1]-a[0] == min){
-                answer = new int[]{a[0], a[1]};
-                break;
+        int size = sequence.length;
+        for(end=0;end<sequence.length;end++){
+            total+=sequence[end];
+
+            while(end<sequence.length && total>k){
+                total-=sequence[start];
+                start++;
+            }
+            if(total==k){
+                if(size>end-start){
+                    size = end-start;
+                    answer[0] = start;
+                    answer[1] = end;
+                }
             }
         }
         System.out.println(Arrays.toString(answer));
